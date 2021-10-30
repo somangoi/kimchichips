e91249c46bf5f9daa40484e2617ac9b5c779f302
somangoi <somi.c.hwang@gmail.com>
2021-10-31 08:14:57 +0900

    logo added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 function Nav() {
-  return <div>hi</div>;
+  return (
+    <NavContainer>
+      <Logo>
+        <img src="/image/logo.png" alt="logo" />
+      </Logo>
+    </NavContainer>
+  );
 }
 
+const NavContainer = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: ${({ theme }) => theme.colorBackground};
+`;
+
+const Logo = styled.div`
+  width: 100px;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+`;
 export default Nav;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,4 @@
 const theme = {
-  colorMain: "#6741DA",
-  colorTitle: "#7D8288",
-  colorDesc: "#9CA5AD",
-  colorBlue: "#3282F0",
-  colorBabyBlue: "#B1D4E0",
-  colorDarkBlue: "#145DA0",
-  colorBackground: "#E5E5E5",
-  notoSans: "'Noto Sans KR', sans-serif",
+  colorBackground: "#e5e5e5",
 };
 export default theme;
